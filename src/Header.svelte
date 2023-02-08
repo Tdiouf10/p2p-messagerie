54ddce76d0338d5a62dbc965cae6da9017cc00b9
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-02-08 10:27:06 +0100

    Initial commit

---
+++ src/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>Messagerie</h1>
+  {#if $username}
+    <div>
+      <span>Bonjour <strong>{$username}</strong></span>
+    </div>
+    <button on:click={signout}>Déconnexion</button>
+  {/if}
+</header>
---
+++ src/Login.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { user } from './user';
+
+  let username;
+  let password;
+
+  function login() {
+    user.auth(username, password, ({ err }) => err && alert(err));
+  }
+
+  function signup() {
+    user.create(username, password, ({ err }) => {
+      if (err) {
+        alert(err);
+      } else {
+        login();
+      }
+    });
+  }
+</script>
+
+<label for="username">Pseudo</label>
+<input bind:value={username} minlength="3" maxlength="16" />
+
+<label for="password">Mot de passe</label>
+<input bind:value={password} type="password" />
+
+<button on:click={login}>Connexion</button>
+<button on:click={signup}>Inscription</button>
+  
